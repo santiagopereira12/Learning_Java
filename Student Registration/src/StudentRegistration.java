536e4695d2aed979c721a41a49fe3a71eb5efc99
santiagopereira12 <spereira98@misena.edu.co>
2025-07-31 10:39:00 -0500

    Asignación de estudiantes

--- Student Registration/src/StudentRegistration.java
+++ Student Registration/src/StudentRegistration.java
@@ -10,7 +10,7 @@ public class StudentRegistration {
         do {
             System.out.println("-----------//MENU//-----------");
             System.out.println("Bienvenido al sistema de gestion de usuarios\nescoja la opción que desea realizar.");
-            System.out.println("1. Listar Estudiantes\n2. Registro de estudiante\n3. Listar Materias\n4.Asignar Estudiantes a materias\n5.Listar Estudiantes con sus Materias.\n6. Salir.");
+            System.out.println("1. Listar Estudiantes\n2. Registro de estudiante\n3. Listar Materias\n4. Asignar Estudiantes a materias\n5. Listar Estudiantes con sus Materias.\n6. Salir.");
             opc = sc.nextInt();
             switch (opc) {
                 case 1:
@@ -30,6 +30,14 @@ public class StudentRegistration {
                     break;
                 case 4:
                     System.out.println("---//Asignación de estudiantes//---");
+                    sc.nextLine();
+                    System.out.println(subject.list());
+                    System.out.print("Digite la materia al que quiere asignar un estudiante: ");
+                    String assign = sc.nextLine();
+                    System.out.println(student.list());
+                    System.out.print("Digite el nombre del estudiante a registrar: ");
+                    String studentRegister = sc.nextLine();
+                    System.out.println("Nombre del estudiante registrado: "+studentRegister+ ", asignado a: "+assign);
                     break;
                 case 5:
                     System.out.println("---//Listado de Estudiantes asignados//---");
