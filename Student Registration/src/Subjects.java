58c7e9901b056d19c0bdd4991fa6269fe7ef1d69
santiagopereira12 <spereira98@misena.edu.co>
2025-08-01 11:04:01 -0500

    Implementación de ArrayList en materias

--- Student Registration/src/Subjects.java
+++ Student Registration/src/Subjects.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+
 public class Subjects {
+    private ArrayList<String> materias = new ArrayList<>();
+
+    public Subjects(){
+        materias.add("Programación");
+        materias.add("Diseño");
+        materias.add("Matematicas");
+        materias.add("Ingles");
+        materias.add("Analitica");
+    }
     public String list(){
-        String lista = "Esta es la lista de materias.";
-        return lista;
+        if (materias.isEmpty()){
+            return "No hay materias para asignar.";
+        }
+
+        StringBuilder lista = new StringBuilder("Lista de materias:\n");
+        for (int i = 0; i < materias.size(); i++){
+            lista.append((i+1)).append(". ").append(materias.get(i)).append("\n");
+        }
+        return lista.toString();
     }
 }
