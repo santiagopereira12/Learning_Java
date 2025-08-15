33034d18ee2263cafbca8da3c7d78891dc1a7a9c
santiagopereira12 <spereira98@misena.edu.co>
2025-08-15 12:07:59 -0500

    Casting de tipos de datos en Java

--- java-poo/src/platzi/play/Main.java
+++ java-poo/src/platzi/play/Main.java
@@ -15,12 +15,21 @@ public class Main {
         pelicula.fechaEstreno = LocalDate.of(2007,10,6);
         pelicula.genero = "Aventura";
         pelicula.calificar(4.5);
+        pelicula.duracion = 120;
+
+        long duracionLong = pelicula.duracion;
+        int calificacionInt = (int) pelicula.calificacion;
+        int numeroPremios = (int) Long.parseLong(String.valueOf(25));
 
         Usuario usuario = new Usuario();
         usuario.nombre = "Santiago";
         usuario.fechaRegistro = LocalDateTime.of(2025,8,11,11,42,50);
 
-        //System.out.println(pelicula.obtenerFichaTecnica());
+        System.out.println(pelicula.obtenerFichaTecnica());
+        System.out.println("Duración: "+duracionLong);
+        System.out.println("Calificación: "+calificacionInt);
+        System.out.println("Premios: "+numeroPremios);
+        System.out.println("\n/------------/USUARIO/-------------/");
         usuario.ver(pelicula);
         System.out.println(usuario.fechaRegistro);
 
