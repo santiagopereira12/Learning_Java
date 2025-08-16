56440d09d356b4b93c25b9f344733d21c1c67775
santiagopereira12 <spereira98@misena.edu.co>
2025-08-16 09:59:01 -0500

    Atributos y métodos estáticos en clases utilitarias de Java

--- java-poo/src/platzi/play/Main.java
+++ java-poo/src/platzi/play/Main.java
@@ -1,6 +1,7 @@
 package platzi.play;
 import platzi.play.contenido.Pelicula;
 import platzi.play.plataforma.Usuario;
+import platzi.play.util.ScannerUtils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,37 +11,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Pelicula pelicula = new Pelicula();
-        pelicula.titulo = "Narnia";
-        pelicula.fechaEstreno = LocalDate.of(2007,10,6);
-        pelicula.genero = "Aventura";
-        pelicula.calificar(4.5);
-        pelicula.duracion = 120;
+        String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
+        String genero = ScannerUtils.capturarTexto("Genero del contenido");
+        int duracion = ScannerUtils.capturarNumero("duración del contenido");
+        double calificacion = ScannerUtils.capturarDecimal("calificacion del contenido");
 
-        long duracionLong = pelicula.duracion;
-        int calificacionInt = (int) pelicula.calificacion;
-        int numeroPremios = (int) Long.parseLong(String.valueOf(25));
+        Pelicula pelicula = new Pelicula();
+        pelicula.titulo = nombre;
+        pelicula.fechaEstreno = LocalDate.of(2007,10,6);
+        pelicula.genero = genero;
+        pelicula.calificar(calificacion);
+        pelicula.duracion = duracion;
+
+        System.out.println(pelicula.obtenerFichaTecnica());
 
         Usuario usuario = new Usuario();
         usuario.nombre = "Santiago";
         usuario.fechaRegistro = LocalDateTime.of(2025,8,11,11,42,50);
 
-        System.out.println(pelicula.obtenerFichaTecnica());
-        System.out.println("Duración: "+duracionLong);
-        System.out.println("Calificación: "+calificacionInt);
-        System.out.println("Premios: "+numeroPremios);
         System.out.println("\n/------------/USUARIO/-------------/");
         usuario.ver(pelicula);
         System.out.println(usuario.fechaRegistro);
 
-        /*
-        System.out.println("¡Platzi Play!");
-        System.out.println("¡Bienvenido!\nDinos como te llamas:");
-        String nombre = sc.nextLine();
-
-        System.out.println(nombre +" ¿Qué edad tienes? ");
-        int edad = sc.nextInt();
-
-        System.out.println("Bienvenido "+nombre+" con tu edad: "+edad+". Puedes ver contenido.");*/
     }
 }
