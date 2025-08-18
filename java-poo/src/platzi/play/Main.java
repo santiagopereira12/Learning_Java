466ccd246036ab0baa12767cfea1dead1b495b9b
santiagopereira12 <spereira98@misena.edu.co>
2025-08-17 19:18:17 -0500

    Constantes en Java: inmutabilidad con static final

--- java-poo/src/platzi/play/Main.java
+++ java-poo/src/platzi/play/Main.java
@@ -7,7 +7,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
+    public static final String NOMBRE_PLATAFORMA = "PLATZI PLAY";
+    public static final String VERSION = "1.0.0";
     public static void main(String[] args) {
+        System.out.println(NOMBRE_PLATAFORMA + " v"+VERSION);
 
         String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
         String genero = ScannerUtils.capturarTexto("Genero del contenido");
