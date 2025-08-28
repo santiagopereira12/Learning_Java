67fcd5a7f0d708328b98d1ed0578ee06bd4a687a
santiagopereira12 <spereira98@misena.edu.co>
2025-08-27 22:00:32 -0500

    Calificación

--- java-poo/src/platzi/play/Main.java
+++ java-poo/src/platzi/play/Main.java
@@ -65,15 +65,15 @@ public class Main {
 
     private static void cargarPelicula(Plataforma plataforma){
         plataforma.agregar(new Pelicula("Shrek",90,"Animada"));
-        plataforma.agregar(new Pelicula("El Rey León", 88, "Animada"));
+        plataforma.agregar(new Pelicula("El Rey León", 88, "Animada", 4.5));
         plataforma.agregar(new Pelicula("Jurassic Park", 127, "Ciencia Ficción"));
-        plataforma.agregar(new Pelicula("Titanic", 195, "Romance"));
+        plataforma.agregar(new Pelicula("Titanic", 195, "Romance",3.8));
         plataforma.agregar(new Pelicula("Matrix", 136, "Acción"));
-        plataforma.agregar(new Pelicula("Gladiador", 155, "Histórica"));
+        plataforma.agregar(new Pelicula("Gladiador", 155, "Histórica",4.2));
         plataforma.agregar(new Pelicula("Coco", 105, "Animada"));
-        plataforma.agregar(new Pelicula("Avengers: Endgame", 181, "Superhéroes"));
+        plataforma.agregar(new Pelicula("Avengers: Endgame", 181, "Superhéroes",4.8));
         plataforma.agregar(new Pelicula("Avatar", 162, "Ciencia Ficción"));
-        plataforma.agregar(new Pelicula("Interestelar", 169, "Ciencia Ficción"));
+        plataforma.agregar(new Pelicula("Interestelar", 169, "Ciencia Ficción",4));
         plataforma.agregar(new Pelicula("Frozen", 102, "Animada"));
     }
 }
