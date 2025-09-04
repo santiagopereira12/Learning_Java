4f2205ff8e8f63065ced90e420cea2e364eb2147
santiagopereira12 <spereira98@misena.edu.co>
2025-09-04 17:12:48 -0500

    Records para simplificar código inmutable en Java

--- java-poo/src/platzi/play/Main.java
+++ java-poo/src/platzi/play/Main.java
@@ -1,6 +1,7 @@
 package platzi.play;
 import platzi.play.contenido.Genero;
 import platzi.play.contenido.Pelicula;
+import platzi.play.contenido.ResumenContenido;
 import platzi.play.excepcion.PeliculaExistenteException;
 import platzi.play.plataforma.Plataforma;
 import platzi.play.plataforma.Usuario;
@@ -55,8 +56,8 @@ public class Main {
 
                 }
                 case MOSTRAR_TODO ->{
-                    List<String> titulos = plataforma.getTitulos();
-                    titulos.forEach(System.out::println);
+                    List<ResumenContenido> contenidosResumidos = plataforma.getResumenes();
+                    contenidosResumidos.forEach(resumen -> System.out.println(resumen.toString()));
                 }
                 case BUSCAR_POR_TITULO -> {
                     String nombreBuscado = ScannerUtils.capturarTexto("titulo del contenico: ");
