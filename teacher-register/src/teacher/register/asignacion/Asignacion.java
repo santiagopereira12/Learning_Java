0228185fb56ddcc095d81ef6fccbe0dc27790008
santiagopereira12 <spereira98@misena.edu.co>
2025-10-03 21:00:44 -0500

    Nueva Clase para Asignar profesorea a cada materia

---
+++ teacher-register/src/teacher/register/asignacion/Asignacion.java
@@ -0,0 +1,8 @@
+package teacher.register.asignacion;
+
+public class Asignacion {
+
+    public String Asignar(){
+        return "Aqui se can asignar los profesores a las diferentes mateiras.";
+    }
+}
