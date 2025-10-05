b5fe66057aef787e08265dec08116d21f5f5b76c
santiagopereira12 <spereira98@misena.edu.co>
2025-10-04 19:31:04 -0500

    Se crea los metodos de asignación

--- teacher-register/src/teacher/register/asignacion/Asignacion.java
+++ teacher-register/src/teacher/register/asignacion/Asignacion.java
@@ -16,4 +16,32 @@ public class Asignacion {
         this.profesor = profesor;
         this.asignaturas = asignaturas;
     }
+
+    public static void asignar(Profesor profesor, Asignaturas asignatura){
+        Asignacion nueva = new Asignacion(profesor, asignatura);
+        listaAsignacion.add(nueva);
+        System.out.println("Se realizo la asignación del profesor: "+profesor+ ", a la materia: " +asignatura.getNombre());
+    }
+
+    public static void asignacionAutomatica(List<Profesor> profesores, List<Asignaturas> asignaturas){
+        for (Profesor profesor : profesores){
+            String carrera = profesor.getCarrera().toLowerCase();
+
+            for (Asignaturas asignatura: asignaturas){
+                String nombreMateria = asignatura.getNombre().toLowerCase();
+
+                if (carrera.contains("matematic") && nombreMateria.contains("matem")){
+                    asignar(profesor, asignatura);
+                } else if (carrera.contains("física") && nombreMateria.contains("física")) {
+                    asignar(profesor, asignatura);
+                } else if (carrera.contains("sistema") && nombreMateria.contains("informática") || nombreMateria.contains("tecnología")) {
+                    asignar(profesor, asignatura);
+                } else if (carrera.contains("lengua") && nombreMateria.contains("lengua")) {
+                    asignar(profesor, asignatura);
+                } else if (carrera.contains("psicó") && nombreMateria.contains("filosofía")) {
+                    asignar(profesor, asignatura);
+                }
+            }
+        }
+    }
 }
