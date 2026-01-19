3f2452f7cd7295f704bb9b039a69b3b3ef769857
santiagopereira12 <spereira98@misena.edu.co>
2026-01-19 09:02:30 -0500

    Login Form

--- Motor Banca/src/motor/regla/banca/Main.java
+++ Motor Banca/src/motor/regla/banca/Main.java
@@ -7,7 +7,12 @@ import motor.regla.banca.service.RegisterService;
 import motor.regla.banca.util.ScannerUtil;
 
 public class Main {
+    private static final String REGISTER = "A";
+    private static final String LOGIN = "B";
+    private static final String LOGOUT = "C";
+
     public static void main(String[] args) {
+        String login;
         System.out.println("Hola Mundo Regla Función Banca");
         UserRepository userRepository = new UserRepository();
         RegisterService registerService  = new RegisterService(userRepository);
@@ -16,13 +21,36 @@ public class Main {
         registerService.registreMassUsers();
         userRepository.getUsers();
 
-        //Crear Usuario
-        String name = ScannerUtil.capturarTexto("Enter Username");
-        int age = ScannerUtil.capturarNumero("Enter age");
-        int doc = ScannerUtil.capturarNumero("Enter document number");
-        User user = new User(name, age, doc);
-        User create = registerService.createUser(user);
-        System.out.println("Usuario Creado:" +create);
+        do {
+            login = ScannerUtil.capturarTexto("""
+                    Enter the option you wish to perfom.
+                    A) Register.
+                    B) Login.
+                    C) Logout.
+                    """).trim().toUpperCase();
+
+            switch (login) {
+                case REGISTER -> {
+                    String name = ScannerUtil.capturarTexto("Enter Username");
+                    int age = ScannerUtil.capturarNumero("Enter age");
+                    int doc = ScannerUtil.capturarNumero("Enter document number");
+                    User user = new User(name, age, doc);
+                    User register = registerService.createUser(user);
+                    System.out.println("Succesfully registered user." + register);
+                }
+                case LOGIN -> {
+                    int numDoc = ScannerUtil.capturarNumero("Enter your Number Document");
+                    System.out.println("User Login");
+                }
+                case LOGOUT -> {
+                    System.out.println("Leaving the system");
+                }
+                default -> {
+                    System.out.println("Wrong option");
+                }
+            }
+        }while (!login.equals(LOGOUT));
+
 /*
         //Crear la cuenta.
         int number = ScannerUtil.capturarNumero("Enter the account number");
